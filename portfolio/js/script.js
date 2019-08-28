258b90c5b6cb69d7263f546a7bf64b33b964d183
AravindKS <aravindvasan1.github.com>
2019-08-28 23:34:23 +0530

    8/28
    menu-toggle

---
+++ portfolio/js/script.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+
+    /* Mobile nav */
+    $('.js--mob-nav').click(function(){
+        
+        let nav = $('.js--main-nav');
+        
+        nav.slideToggle(200);
+        
+    });
+
+} );
