8ecda79c2f0215f7252fe9f9c5e9870b5116e73f
AravindKS <aravindvasan1.github.com>
2019-08-29 19:20:34 +0530

    29/8
    updated

--- pro3/js/script.js
+++ pro3/js/script.js
@@ -1,28 +1,31 @@
-$(document).ready(function() {
+( function($) { 
+    
+    $(document).ready(function() {
 
-    /* Mobile nav */
-    $('.js--mob-nav').click(function(){
-        
-        let nav = $('.js--main-nav');
-        
-        nav.slideToggle(200);
-        
-    });
+        /* Mobile nav */
+        $('.js--mob-nav').click(function(){
 
-    /* Navigation scroll */
-    $(function() {
-        $('a[href*="#"]:not([href="#"])').click(function() {
-          if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-            var target = $(this.hash);
-            target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-            if (target.length) {
-              $('html, body').animate({
-                scrollTop: target.offset().top
-              }, 1000);
-              return false;
-            }
-          }
+            let nav = $('.js--main-nav');
+
+            nav.slideToggle(200);
+
         });
-      });
 
-} );
+        /* Navigation scroll */
+        $(function() {
+            $('a[href*="#"]:not([href="#"])').click(function() {
+              if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+                var target = $(this.hash);
+                target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+                if (target.length) {
+                  $('html, body').animate({
+                    scrollTop: target.offset().top
+                  }, 1000);
+                  return false;
+                }
+              }
+            });
+          });
+
+    } );
+} ) (jQuery);
