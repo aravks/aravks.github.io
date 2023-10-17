1432ed8d5ee389f65305b4980fff05deb9f99687
Aravind K S <aravindvasan1@gmail.com>
2023-10-17 13:07:43 +0530

    Updated animation bug

--- portfolio/index.js
+++
@@ -1,8 +0,0 @@
-function callme() {
-  if (document.getElementById('showme').innerHTML == 'Clicked') {
-    document.getElementById('showme').innerHTML = 'Not Clicked';
-  } else {
-    document.getElementById('showme').innerHTML = 'Clicked';
-  }
-  console.log(document.getElementById('showme').innerHTML);
-}
